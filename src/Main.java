014e95e1090b24bd622637ee5b5cbc1e71b0d81d
IskanderV1 <ngk5555@yandex.ru>
2022-07-25 20:30:25 +0300

    The versus battle

---
+++ src/Main.java
@@ -0,0 +1,182 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Rules:");
+        System.out.println("Write each word with a small letter and without spelling errors!");
+        System.out.println();
+        System.out.println("Write (continue) to start Versus battl");
+        Scanner continu = new Scanner(System.in);
+        String one = continu.nextLine();
+        ;
+        if (one.equals("continue")) System.out.println("Гавно");
+        else {
+            return;
+        }
+
+        Scanner continu2 = new Scanner(System.in);
+        String two = continu2.nextLine();
+        if (two.equals("залупа")) System.out.println("пенис");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu3 = new Scanner(System.in);
+        String three = continu3.nextLine();
+        if (three.equals("хер")) System.out.println("давалка");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu4 = new Scanner(System.in);
+        String four = continu4.nextLine();
+        if (four.equals("хуй")) System.out.println("блядина");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu5 = new Scanner(System.in);
+        String five = continu4.nextLine();
+        if (five.equals("головка")) System.out.println("шлюха");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu6 = new Scanner(System.in);
+        String six = continu6.nextLine();
+        if (six.equals("жопа")) System.out.println("член");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu7 = new Scanner(System.in);
+        String seven = continu7.nextLine();
+        if (seven.equals("еблан")) System.out.println("петух");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu8 = new Scanner(System.in);
+        String eight = continu8.nextLine();
+        if (eight.equals("мудила")) System.out.println("рукоблуд");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu9 = new Scanner(System.in);
+        String nine = continu9.nextLine();
+        if (eight.equals("ссанина")) System.out.println("очко");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu10 = new Scanner(System.in);
+        String ten = continu10.nextLine();
+        if (eight.equals("блядун")) System.out.println("вагина");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu11 = new Scanner(System.in);
+        String eleven = continu11.nextLine();
+        if (eight.equals("сука")) System.out.println("ебланище");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu12 = new Scanner(System.in);
+        String twelve = continu12.nextLine();
+        if (eight.equals("влагалище")) System.out.println("пердун");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu13 = new Scanner(System.in);
+        String a13 = continu13.nextLine();
+        if (eight.equals("дрочила")) System.out.println("пидор");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu14 = new Scanner(System.in);
+        String a14 = continu14.nextLine();
+        if (eight.equals("пизда")) System.out.println("туз");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu15 = new Scanner(System.in);
+        String a15 = continu15.nextLine();
+        if (eight.equals("малафья")) System.out.println("гомик");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu16 = new Scanner(System.in);
+        String a16 = continu16.nextLine();
+        if (eight.equals("мудила")) System.out.println("пилотка");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu17 = new Scanner(System.in);
+        String a17 = continu17.nextLine();
+        if (eight.equals("манда")) System.out.println("анус");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu18 = new Scanner(System.in);
+        String ф18 = continu18.nextLine();
+        if (eight.equals("вагина")) System.out.println("путана");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu19 = new Scanner(System.in);
+        String e19 = continu19.nextLine();
+        if (eight.equals("педрила")) System.out.println("шалава");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu20 = new Scanner(System.in);
+        String e20 = continu20.nextLine();
+        if (eight.equals("хуила")) System.out.println("мошонка");
+        else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+
+        Scanner continu21 = new Scanner(System.in);
+        String e21 = continu21.nextLine();
+        if (eight.equals("елда")) {
+            System.out.println("РАУНД!");
+            System.out.println("CHEL HOROSH!");
+        } else {
+            System.out.println("You lost, try again!");
+            return;
+        }
+    }
+}
+
+
